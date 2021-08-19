57bf4440fa8828f27728902afa69eaa136d218c2
Carlos-SSH <carlos.anezeh@me.com>
2021-08-18 20:01:45 -0500

    Created an object called todos with the basic requirements like: description, completed, and provided a temporary index.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
-import "./style.css";
+import './style.css';
 
-import createList from "./createList";
-
-const run = () => {
-  createList();
-}
-
-run();
+let todos = [
+  {
+    desctiption: "Take a shower",
+    completed: false,
+    index: 1
+  },
+  {
+    desctiption: "Read my new book",
+    completed: false,
+    index: 2
+  },
+  {
+    description: "Wash dishes",
+    completed: true,
+    index: 3
+  }
+  {
+    desctiption: "Go at the supermarket",
+    completed: true,
+    index: 4
+  }
+];
