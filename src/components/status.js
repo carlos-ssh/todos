75074cce38d3b322123c510018528f484631157e
Carlos-SSH <carlos.anezeh@me.com>
2021-08-18 22:45:00 -0500

    Add a validation if it's completed a todo task.

--- src/components/status.js
+++ src/components/status.js
@@ -1,5 +1,22 @@
 module default class isCompleted {
   static updateLocalStorage(list) {
     const data = JSON.stringify(list);
+    localStorage.setItem('todo', data);
+  }
+
+  static completeTodo(list) {
+    this.checkboxx = document.querySelectorAll('.checbox');
+    this.checkboxx.forEach((checkbox, index) => {
+      checkbox.addEventListener('change', function checked() {
+	if (this.checked) {
+	  checkbox.parentNode.querySelector('.text').classList.add('trought-line');
+	  list[index].completed = true;
+	} else {
+	  checkbox.parentNode.querySelector('.text').classList.remove('trought-line');
+	  list[index].completed = false;
+	}
+	isCompleted.updateLocalStorage(list);
+      });
+    });
   }
 }
