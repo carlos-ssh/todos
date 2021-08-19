2d12f9c43610d7c2ef412e2460e92074ed255ba2
Carlos-SSH <carlos.anezeh@me.com>
2021-08-19 12:52:48 -0500

    Fixed issues in the list.

--- src/index.js
+++ src/index.js
@@ -31,16 +31,16 @@ const displayTodos = () => {
     const checkItem = item.completed ? 'checked' : '';
     const lineTrought = item.completed ? 'trought-line' : '';
     item.index = index;
-    const listElement =   
-     `<li>` +
-	`<input type="checkbox" class="checkbox" id="checkbox-${item.index}" ${checkItem}> ` +
-	  `<h4 class="text ${lineTrought} trought-line"> ${item.description} </h4>` +
-	  `<img src="https://img.icons8.com/ios-glyphs/30/000000/menu-2.png" alt="Vertical Menu">` +
-      `</li>`;
-    ul[0].insertAdjacentHTML(
-              'beforeend',
-      listElement      ); 
+    ul[0].innerHTML += `
+      <li>
+	<input type="checkbox" class="checkbox" id="checkbox-${item.index}" ${checkItem}>
+	  <h4 class="text ${lineTrought}">
+	    ${item.description}
+	  </h4>
+	  <img class="drop" src="https://img.icons8.com/ios-glyphs/30/000000/menu-2.png" alt="vertical menu"/>
+      </li>`;
   });
+ 
   Completed.completeTodo(todos);
 }
 
